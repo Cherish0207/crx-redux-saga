d57813a2e581eeee7f53607fca14a1a2dbfc24f4
Cherish0207 <cheshire_1207@foxmail.com>
2021-06-20 21:13:39 +0800

    支持产出iterator

--- src/redux-saga/runSaga.js
+++ src/redux-saga/runSaga.js
@@ -2,20 +2,25 @@ import * as effectTypes from "./effectTypes";
 export default function runSaga(env, saga) {
   console.log("runSaga");
   let { channel, dispatch } = env;
-  let it = saga();
+  let it = typeof saga === "function" ? saga() : saga;
   function next(value) {
     let { value: effect, done } = it.next(value);
     if (!done) {
-      switch (effect.type) {
-        case effectTypes.TAKE:
-          channel.channelTake(effect.actionType, next);
-          break;
-        case effectTypes.PUT:
-          dispatch(effect.action);
-          next();
-          break;
-        default:
-          break;
+      if (typeof effect[Symbol.iterator] === "function") {
+        runSaga(env, effect);
+        next();
+      } else {
+        switch (effect.type) {
+          case effectTypes.TAKE:
+            channel.channelTake(effect.actionType, next);
+            break;
+          case effectTypes.PUT:
+            dispatch(effect.action);
+            next();
+            break;
+          default:
+            break;
+        }
       }
     }
   }
